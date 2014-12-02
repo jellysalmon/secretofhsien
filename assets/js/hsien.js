9b1a7d09cf3a70fc4dc5b69a8e76468096a0d0af
jellysalmon <joey.samaniego@gmail.com>
2014-12-01 22:42:40 -0800

    added images

--- assets/js/hsien.js
+++ assets/js/hsien.js
@@ -1,9 +1,27 @@
-function Ninja() {
-  this.x = 300;
-  this.y = 300;
+$(document).ready(function(){
+  n = new Hsienko();
+})
+
+function Hsienko() {
+  this.x = 0;
+  this.y = 0;
+  this.initDisplay();
 }
 
-Ninja.prototype.updateDisplay = function() {
+
+Hsienko.prototype.initDisplay = function() {
+  this.$hsienko = $("<div class='hsienko'></div>")
+  $('#stage').append(this.$hsienko);
+  this.$hsienko.css('position', 'relative');
+  this.$hsienko.css('top', this.x);
+  this.$hsienko.css('left', this.y);
+  this.$hsienko.css('height', 59);
+  this.$hsienko.css('width', 59);
+  this.$hsienko.css('background-image', 'url("assets/images/PF_Hsien-Ko.gif")');
+  this.$hsienko.css('background-repeat', 'no-repeat');
+}
+
+Hsienko.prototype.updateDisplay = function() {
   console.log(this.x);
   console.log(this.x);
 }
@@ -14,3 +32,4 @@ Game = {
     console.log("Hello");
   }
 }
+
