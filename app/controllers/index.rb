1ddce93b62b4cf9d3e82481f9e5591a3af863fc9
jellysalmon <joey.samaniego@gmail.com>
2014-12-05 15:43:26 -0800

    fixed google fonts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,22 +5,22 @@ get '/' do
 end
 
 get '/login' do
-  client_id = "75d5633l2czc3k"
-  redirect "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=#{client_id}&state=HSIEN45453sdffef424&redirect_uri=http://localhost:9393/logged_in"  
+  client_id = ENV['CLIENT_ID']
+  redirect "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=#{client_id}&state=HSIEN45453sdffef424&redirect_uri=https://calm-earth-8611.herokuapp.com/logged_in"  
 end
 
 get "/logged_in" do
   code_response = params['code']
-  client_id = "75d5633l2czc3k"
+  
   # p "SECRET_KEY"
   # p ENV['SECRET_KEY']
 
 
   token_response = HTTParty.post( "https://www.linkedin.com/uas/oauth2/accessToken", body: {
     code: code_response,
-    client_id: client_id,
+    client_id: ENV['CLIENT_ID'],
     client_secret: ENV['SECRET_KEY'],
-    redirect_uri: "http://localhost:9393/logged_in",
+    redirect_uri: "https://calm-earth-8611.herokuapp.com/logged_in",
     grant_type: "authorization_code"
   })
 
@@ -30,6 +30,7 @@ get "/logged_in" do
 
   user = User.find_or_create_by(name: @linkedin_response["person"]["first_name"])
 
+  session[:user.id]
   redirect "/game/#{user.id}"
 end
 
